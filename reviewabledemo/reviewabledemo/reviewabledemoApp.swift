d17245fa5e1ac321fbfa207fb4e4812c2d8b53f8
Alok Kumar <alok.kumar@deltatre.com>
2024-09-23 19:09:01 +0530

    created sample xcode project

---
+++ reviewabledemo/reviewabledemo/reviewabledemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  reviewabledemoApp.swift
+//  reviewabledemo
+//
+//  Created by Alok Kumar on 23/09/24.
+//
+
+import SwiftUI
+
+@main
+struct reviewabledemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
