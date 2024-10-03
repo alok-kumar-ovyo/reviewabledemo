0fe01b956cfef3c606cdf52fd279d439b5e24cac
Alok Kumar <alok.kumar@deltatre.com>
2024-10-03 11:07:00 +0530

    code reverted

--- reviewabledemo/reviewabledemo/ContentView.swift
+++ reviewabledemo/reviewabledemo/ContentView.swift
@@ -8,49 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
-    
-    @State private var isAddTapped = false
-    @State private var isModifyTapped = false
-    
     var body: some View {
-        GeometryReader { geo in
-            VStack {
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundStyle(.tint)
-                Text("Hello, world!")
-                
-                Spacer()
-                
-                HStack(spacing: 0) {
-                    Button {
-                        isAddTapped.toggle()
-                    } label: {
-                        Text("Add")
-                            .frame(width: geo.size.width/2, height: 46)
-                    }
-                    
-                    Button {
-                        isModifyTapped.toggle()
-                    } label: {
-                        Text("Modify")
-                            .frame(width: geo.size.width/2, height: 46)
-                    }
-                }
-                .frame(width: geo.size.width)
-            }
-            .frame(width: geo.size.width, height: geo.size.height)
-            .alert("Add CTA Tapped", isPresented: $isAddTapped) {
-                Button("OK", role: .cancel) {
-                    print("here @ Add")
-                }
-            }
-            .alert("Modify CTA Tapped", isPresented: $isModifyTapped) {
-                Button("OK", role: .cancel) {
-                    print("here @ Modify")
-                }
-            }
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
         }
+        .padding()
     }
 }
 
